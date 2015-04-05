34ce35e4d02702fc3271ec01a6b729803bedb739
sumitlni <sumit@loudnoiseinc.com>
2015-04-05 14:57:37 +0000

    Updated index.html

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var content = fs.readFileSync(index.html,'utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(content)
 })
 
 app.listen(app.get('port'), function() {
